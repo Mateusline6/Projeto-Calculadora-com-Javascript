02a851c5d21febc026cac12d8f0c82c8ca4f24c2
Mateus Silva <ryuksilva@gmail.com>
2020-01-28 16:08:27 -0300

    Modificação de data e hora

--- projeto-calculadora-javascript/scripts/controler/CalcController.js
+++ projeto-calculadora-javascript/scripts/controler/CalcController.js
@@ -1,41 +1,92 @@
 class CalcController {
 
     constructor() {
-        this._displayCalc = "0";
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
     }
 
     initialize() {
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        this.setDisplayDateTime();
 
-        displayCalcEl.innerHTML = "4567";
-        dateEl.innerHTML = "28/01/2020";
-        timeEl.innerHTML ="00:00";
+        setInterval(() => {
+
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+                day: "2-digit",
+                month: "long",
+                year: "numeric"
+                
+            });
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        }, 1000);
+
+        //So teste
+        /*setTimeout(()=>{
+
+            clearInterval(internal);
+
+        }, 10000);*/
 
     }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
+
+
+
+
+    get displayTime() {
+
+        return this._timeEl.innerHTML;
+
+    }
+
+    set displayTime(value) {
+
+        return this._timeEl.innerHTML = value;
+    }
+
+
+    get displayDate() {
+
+        return this._dateEl.innerHTML;
+
+    }
+    set displayDate(value) {
+
+        return this._dateEl.innerHTML = value;
+
+    }
+
+
 
 
 
     get displayCalc() {
 
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
 
 
     }
 
-    set displayCalc(valor) {
+    set displayCalc(value) {
 
-        this._displayCalc = valor;
+        this._displayCalcEl.innerHTML = value;
 
     }
 
     get currentDate() {
 
-        return this._currentDate;
+        return new Date();
 
     }
 
